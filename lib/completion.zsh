a71ff2e23104a3635520802ffc79d3167fd1db7e
Alex Graber <ahgraber@ninerealmlabs.com>
2021-10-30 17:43:57 -0400

    continuing refinement

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -43,7 +43,7 @@ zstyle ':completion:*' group-name ''
 zstyle ':completion:*' verbose yes
 
 # Fuzzy match mistyped completions.
-zstyle ':completion:*' completer _expand _complete _ignored _match _approximate
+zstyle ':completion:*' completer _expand _complete _match _approximate
 zstyle ':completion:*:match:*' original only
 zstyle ':completion:*:approximate:*' max-errors 1 numeric
 

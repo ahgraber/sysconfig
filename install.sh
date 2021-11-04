4f0b4c353517d47855cb82ead636f6d0cd09657c
Alex Graber <agraber@pmi.org>
2021-11-04 15:00:45 -0400

    continuing refinement

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ if [[ -d "$ZSH_CONFIG" ]]; then
 
     if [[ "$git_select" =~ $yesexpr ]]; then
       echo -e "\nUpdating (Will attempt to reapply any local changes)..."
-      cd "$ZSH_CONFIG" # || exit
+      cd "$ZSH_CONFIG" || exit
       echo -e "\nStashing local changes (stash)..."
       git stash
       echo -e "\nUpdating (pull --rebase)..."

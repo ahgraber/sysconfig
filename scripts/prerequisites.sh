295ba267ebec93531e3943df2112f4dff8307f27
Alex Graber <24922003+ahgraber@users.noreply.github.com>
2021-11-11 21:51:27 -0500

    Update prerequisites.sh

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -40,23 +40,10 @@ fi
 mkdir -p "$HOME/Library/Fonts"
 
 # meslo nf
-echo "Installing Meslo NF to ~/Library/Fonts..."
-cp ./font/*.ttf "$HOME/Library/Fonts"
-
-# install hack nerd font
-echo "Installing Hack Nerd Font to ~/Library/Fonts..."
-curl -fsSL https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip > hackfont.zip
-unzip hackfont.zip -d hackfont
-cp hackfont/ttf/*.ttf "$HOME/Library/Fonts"
-rm -rf hackfont.zip hackfont
-
-# install awesome-terminal-fonts
-echo "Installing awesome-terminal-fonts to ~/Library/Fonts..."
-curl -fsSL https://github.com/gabrielelana/awesome-terminal-fonts/archive/refs/tags/v1.1.0.zip > termfont.zip
-unzip termfont.zip -d termfont/
-cp termfont/awesome-terminal-fonts-1.1.0/build/*.ttf "$HOME/Library/Fonts"
-mkdir -p "$HOME/.fonts"; cp termfont/awesome-terminal-fonts-1.1.0/build/*.sh "$HOME/.fonts"
-rm -rf termfont.zip termfont
+if ! ls ~/Library/Fonts/Meslo* &> /dev/null; then
+  echo "Installing Meslo NF to ~/Library/Fonts..."
+  cp ./font/*.ttf "$HOME/Library/Fonts"
+fi
 
 # install mambaforge
 # brew install --cask mambaforge

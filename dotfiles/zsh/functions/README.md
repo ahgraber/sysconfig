9cea16d656cf7c1bb51513cb72584b62fe87c10d
ahgraber <ahgraber@ninerealmlabs.com>
2023-04-06 20:50:40 -0400

    zshrc and dotfiles integrations

--- dotfiles/zsh/functions/README.md
+++ dotfiles/zsh/functions/README.md
@@ -1,3 +1,3 @@
 # zsh profile functions
 
-These functions are sourced in the `~/.zshrc` and cannot assume access to `/bin` or `/src` when they are called.
+These functions are sourced in the `~/.zshrc` and cannot assume access to `/bin` when they are called.
--- dotfiles/zsh/functions/get_project_dir.sh
+++ dotfiles/zsh/functions/get_project_dir.sh
@@ -1,41 +1,44 @@
 #! /usr/bin/env bash
 
-set -o errexit
+# set -o errexit
 # set -o nounset
 set -o pipefail
 # set -o xtrace
 
+# get parent dir if file or use dir if dir
+_get_file_dir() {
+  local fpath; fpath="${1:-$(pwd)}" || { echo "Error getting current directory"; exit 1; }
+  if [[ -d "$fpath" ]]; then
+    echo "$fpath"
+  elif [[ -f "$fpath" ]]; then
+    dirname "$fpath"
+  else
+    >&2 echo "Error: '$fpath' not detected as file or directory"
+    return 1
+  fi
+}
+
 # shellcheck disable=SC2120
 _find_project() {
   # Traverse up the directory tree until a project folder is found
-  local dir; dir="$(pwd)"
+  local dir; dir="${1:-$(pwd)}" || { echo "Error getting current directory"; exit 1; }
   while [ "$dir" != "/" ]; do
     # Check for required project files and directories
-    if [ -e "$dir/README.md" ] && [ -d "$dir/bin" ] && [ -d "$dir/dotfiles" ] && [ -d "$dir/src" ]; then
+    if [ -e "$dir/README.md" ] && [ -d "$dir/bin" ] && [ -d "$dir/dotfiles" ]; then
       echo "$dir"
       return 0
     fi
-    dir=$(dirname "$dir")
+    dir="$(dirname "$dir")"
   done
   # If no project folder is found, return an error message
   >&2 echo "Error: Could not identify project directory!"
   return 1
 }
 
-# get parent dir if file or use dir if dir
-if [[ -d "$1" ]]; then
-  dir="$1"
-elif [[ -f "$1" ]]; then
-  dir=$(dirname "$1")
-else
-  >&2 echo "Error: '$1' not detected as file or directory"
-  exit 1
-fi
-
-cd "$dir"
-
+dir="$(_get_file_dir "${1:-"$(pwd)"}")"
+cd "$dir" || exit 1
 if [[ "$(git rev-parse --is-inside-work-tree)" == 'true' ]]; then
   git rev-parse --show-toplevel
 else
-  _find_project
+  _find_project # implied cwd
 fi
